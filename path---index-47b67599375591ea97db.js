cc1856cc3b0af01ec47e6d8262ad61b360567182
maidinh <maidinh@postech.ac.kr>
2018-01-07 13:42:39 +0900

    t

--- path---index-47b67599375591ea97db.js
+++ path---index-47b67599375591ea97db.js
@@ -1,2 +1,2 @@
-webpackJsonp([0x81b8806e4260],{1656:function(e,a){e.exports={data:{site:{siteMetadata:{languages:{defaultLangKey:"en",langs:["en","vi"]}}}},pathContext:{slug:"/",langKey:"en"}}}});
+webpackJsonp([0x81b8806e4260],{1908:function(e,a){e.exports={data:{site:{siteMetadata:{languages:{defaultLangKey:"en",langs:["en","vi"]}}}},pathContext:{slug:"/",langKey:"en"}}}});
 //# sourceMappingURL=path---index-47b67599375591ea97db.js.map
